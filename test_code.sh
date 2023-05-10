4d10405f358d9775a2f9fd4632eca6c1577c7fa9
gajdi88 <70482516+gajdi88@users.noreply.github.com>
2023-05-10 17:31:53 +0100

    use GPIO3

--- test_code.sh
+++ test_code.sh
@@ -1,12 +1,12 @@
 # turn on pin 9 for export
-echo 9 > /sys/class/gpio/export
+echo 3 > /sys/class/gpio/export
 
 # set pin 9 as output
-echo "out" > /sys/class/gpio/gpio9/direction
+echo "out" > /sys/class/gpio/gpio3/direction
 
 
 # turn off
-echo 0 > /sys/class/gpio/gpio9/value
+echo 0 > /sys/class/gpio/gpio3/value
 
 # turn on
-echo 1 > /sys/class/gpio/gpio9/value
+echo 1 > /sys/class/gpio/gpio3/value
